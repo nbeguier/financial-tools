7751abbae53554b3cf90aafba10d8fb3867d9f92
nbeguier <nicolas_beguier@hotmail.com>
2021-11-09 21:51:41 +0100

    Reporter v1.13.0: Add html support

--- reporter.py
+++ reporter.py
@@ -29,7 +29,7 @@ except ImportError:
 # Debug
 # from pdb import set_trace as st
 
-VERSION = '1.12.0'
+VERSION = '1.13.0'
 
 def get_sign(value):
     """
@@ -88,65 +88,59 @@ def load_report(input_file, display_report=True):
                 display.print_report(sub_report, footer=False)
     return report
 
-def report_valorisation(old_report, new_report):
+def report_valorisation(old_report, new_report, html_tag):
     """
     Report the valorisation
     """
     if is_different_and_valid(old_report, new_report, 'valorisation'):
         evo_valorisation = round(100 * (-1 + \
             float(new_report['valorisation']) / float(old_report['valorisation'])), 2)
-        print('Evolution valorisation: {}{} %'.format(
-            get_sign(evo_valorisation), evo_valorisation))
-        print('Evolution valorisation: {} -> {} EUR'.format(
-            old_report['valorisation'], new_report['valorisation']))
+        print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Evolution valorisation{html_tag["bold_out"]}: {get_sign(evo_valorisation)}{evo_valorisation} %{html_tag["li_out"]}')
+        print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Evolution valorisation{html_tag["bold_out"]}: {old_report["valorisation"]} -> {new_report["valorisation"]} EUR{html_tag["li_out"]}')
 
-def report_per(old_report, new_report):
+def report_per(old_report, new_report, html_tag):
     """
     Report the PER
     """
     if is_different_and_valid(old_report, new_report, 'PER'):
         evo_per = round(float(new_report['PER']) - float(old_report['PER']), 1)
-        print('Evolution PER: {}{}'.format(get_sign(evo_per), evo_per))
-        print('Evolution PER: {} -> {}'.format(old_report['PER'], new_report['PER']))
+        print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Evolution PER{html_tag["bold_out"]}: {get_sign(evo_per)}{evo_per}{html_tag["li_out"]}')
+        print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Evolution PER{html_tag["bold_out"]}: {old_report["PER"]} -> {new_report["PER"]}{html_tag["li_out"]}')
         if analysis.per_text(old_report['PER']) != analysis.per_text(new_report['PER']):
-            print('Evolution PER: {} -> {}'.format(
-                analysis.per_text(old_report['PER']),
-                analysis.per_text(new_report['PER'])))
+            print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Evolution PER{html_tag["bold_out"]}: {analysis.per_text(old_report["PER"])} -> {analysis.per_text(new_report["PER"])}{html_tag["li_out"]}')
 
-def report_peg(old_report, new_report):
+def report_peg(old_report, new_report, html_tag):
     """
     Report the PEG
     """
     if is_different_and_valid(old_report, new_report, 'peg'):
         evo_peg = round(float(new_report['peg']) - float(old_report['peg']), 1)
-        print('Evolution PEG: {}{}'.format(get_sign(evo_peg), evo_peg))
-        print('Evolution PEG: {} -> {}'.format(old_report['peg'], new_report['peg']))
+        print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Evolution PEG{html_tag["bold_out"]}: {get_sign(evo_peg)}{evo_peg}{html_tag["li_out"]}')
+        print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Evolution PEG{html_tag["bold_out"]}: {old_report["peg"]} -> {new_report["peg"]}{html_tag["li_out"]}')
 
-def report_benefices(old_report, new_report):
+def report_benefices(old_report, new_report, html_tag):
     """
     Report the benefices
     """
     if is_different_and_valid(old_report, new_report, 'benefices'):
         evo_benef = round(float(new_report['benefices']) - float(old_report['benefices']), 2)
-        print('Evolution benefices: {}{} points'.format(
-            get_sign(evo_benef), evo_benef))
-        print('Evolution benefices: {} -> {}'.format(
-            old_report['benefices'], new_report['benefices']))
+        print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Evolution benefices{html_tag["bold_out"]}: {get_sign(evo_benef)}{evo_benef} points{html_tag["li_out"]}')
+        print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Evolution benefices{html_tag["bold_out"]}: {old_report["benefices"]} -> {new_report["benefices"]}{html_tag["li_out"]}')
 
-def report_rdv(old_report, new_report):
+def report_rdv(old_report, new_report, html_tag):
     """
     Report the Rendez-vous
     """
     if is_different_and_valid(old_report, new_report, 'Prochain rdv'):
-        print('Nouveau rdv: {}'.format(new_report['Prochain rdv']))
+        print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Nouveau rdv{html_tag["bold_out"]}: {new_report["Prochain rdv"]}{html_tag["li_out"]}')
     try:
         struct_time = time.strptime(new_report['Prochain rdv'], '%d/%m/%y')
         if 0 <= (datetime(*struct_time[:6]) - datetime.now()).days <= 3:
-            print('[Reminder] Prochain rdv: {}'.format(new_report['Prochain rdv']))
+            print(f'{html_tag["li_in"]}{html_tag["bold_in"]}[Reminder] Prochain rdv{html_tag["bold_out"]}: {new_report["Prochain rdv"]}{html_tag["li_out"]}')
     except (ValueError, KeyError, TypeError):
         pass
 
-def report_trend(old_report, new_report):
+def report_trend(old_report, new_report, html_tag):
     """
     Report the trending
     """
@@ -154,72 +148,88 @@ def report_trend(old_report, new_report):
         old_trend = analysis.trend(old_report)
         new_trend = analysis.trend(new_report)
         if old_trend['short term'] != new_trend['short term']:
-            print('Tendance court terme: {}/5 -> {}/5'.format(
-                old_trend['short term'], new_trend['short term']))
+            print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Tendance court terme{html_tag["bold_out"]}: {old_trend["short term"]}/5 -> {new_trend["short term"]}/5{html_tag["li_out"]}')
         else:
-            print('Tendance court terme: {}/5'.format(new_trend['short term']))
+            print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Tendance court terme{html_tag["bold_out"]}: {new_trend["short term"]}/5{html_tag["li_out"]}')
         if old_trend['mid term'] != new_trend['mid term']:
-            print('Tendance moyen terme: {}/5 -> {}/5'.format(
-                old_trend['mid term'], new_trend['mid term']))
+            print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Tendance moyen terme{html_tag["bold_out"]}: {old_trend["mid term"]}/5 -> {new_trend["mid term"]}/5{html_tag["li_out"]}')
         else:
-            print('Tendance moyen terme: {}/5'.format(new_trend['mid term']))
+            print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Tendance moyen terme{html_tag["bold_out"]}: {new_trend["mid term"]}/5{html_tag["li_out"]}')
 
-def report_potential(old_report, new_report):
+def report_potential(old_report, new_report, html_tag):
     """
     Report the potential
     """
     if 'potential' in new_report:
         if 'potential' not in old_report:
-            print('[Boursorama] Potentiel 3 mois: {} EUR'.format(
-                new_report['potential']['brsrm']['value']))
-            print('[Fortuneo] Potentiel 3 mois: {} EUR'.format(
-                new_report['potential']['frtn']['value']))
+            print(f'{html_tag["li_in"]}{html_tag["bold_in"]}[Boursorama] Potentiel 3 mois{html_tag["bold_out"]}: {new_report["potential"]["brsrm"]["value"]} EUR{html_tag["li_out"]}')
+            print(f'{html_tag["li_in"]}{html_tag["bold_in"]}[Fortuneo] Potentiel 3 mois{html_tag["bold_out"]}: {new_report["potential"]["frtn"]["value"]} EUR{html_tag["li_out"]}')
         else:
             if 'brsrm' not in old_report['potential'] or (\
                 old_report['potential']['brsrm']['value'] != \
                 new_report['potential']['brsrm']['value']):
-                print('[Boursorama] Potentiel 3 mois: {} -> {} EUR'.format(
-                    old_report['potential']['brsrm']['value'],
-                    new_report['potential']['brsrm']['value']))
+                print(f'{html_tag["li_in"]}{html_tag["bold_in"]}[Boursorama] Potentiel 3 mois{html_tag["bold_out"]}: {old_report["potential"]["brsrm"]["value"]} -> {new_report["potential"]["brsrm"]["value"]} EUR{html_tag["li_out"]}')
             if 'frtn' not in old_report['potential'] or (\
                 old_report['potential']['frtn']['value'] != \
                 new_report['potential']['frtn']['value']):
-                print('[Fortuneo] Potentiel: {} -> {} EUR'.format(
-                    old_report['potential']['frtn']['value'],
-                    new_report['potential']['frtn']['value']))
+                print(f'{html_tag["li_in"]}{html_tag["bold_in"]}[Fortuneo] Potentiel{html_tag["bold_out"]}: {old_report["potential"]["frtn"]["value"]} -> {new_report["potential"]["frtn"]["value"]} EUR{html_tag["li_out"]}')
 
-def diff_report(oldest_file, newer_file, isin_compare):
+def diff_report(oldest_file, newer_file, isin_compare, is_html):
     """
     Compare two report
     """
+    html_tag = {
+        'bold_in': '',
+        'bold_out': '',
+        'li_in': '',
+        'li_out': ''
+    }
+    if is_html:
+        html_tag = {
+            'bold_in': '<b>',
+            'bold_out': '</b>',
+            'li_in': '<li>',
+            'li_out': '</li>'
+        }
+
     old_reports = load_report(oldest_file, display_report=False)
     new_reports = load_report(newer_file, display_report=False)
 
-    print('==============')
+    if is_html:
+        print('<html><body>')
+    else:
+        print('==============')
     for _isin in isin_compare:
+        if is_html:
+            print('<div><ul>')
         if _isin not in old_reports or _isin not in new_reports:
             continue
         old_report = old_reports[_isin]
         new_report = new_reports[_isin]
-        print('ISIN: {}'.format(new_report['isin']))
+        print(f'{html_tag["li_in"]}{html_tag["bold_in"]}ISIN{html_tag["bold_out"]}: {new_report["isin"]}{html_tag["li_out"]}')
         if 'nom' in new_report:
-            print('Nom: {}'.format(new_report['nom']))
+            print(f'{html_tag["li_in"]}{html_tag["bold_in"]}Nom{html_tag["bold_out"]}: {new_report["nom"]}{html_tag["li_out"]}')
 
-        report_valorisation(old_report, new_report)
+        report_valorisation(old_report, new_report, html_tag)
 
-        report_per(old_report, new_report)
+        report_per(old_report, new_report, html_tag)
 
-        report_peg(old_report, new_report)
+        report_peg(old_report, new_report, html_tag)
 
-        report_benefices(old_report, new_report)
+        report_benefices(old_report, new_report, html_tag)
 
-        report_rdv(old_report, new_report)
+        report_rdv(old_report, new_report, html_tag)
 
-        report_trend(old_report, new_report)
+        report_trend(old_report, new_report, html_tag)
 
-        report_potential(old_report, new_report)
+        report_potential(old_report, new_report, html_tag)
 
-        print('==============')
+        if is_html:
+            print('</ul></div>')
+        else:
+            print('==============')
+    if is_html:
+        print('</body></html>')
 
 if __name__ == '__main__':
 
@@ -250,6 +260,8 @@ if __name__ == '__main__':
         help='... this newer file')
     DIFF_PARSER.add_argument('-i', '--isin', action='store',\
         help='Specific ISIN to compare', default='')
+    DIFF_PARSER.add_argument('--html', action='store_true',\
+        help='Output in HTML format', default=False)
 
     ARGS = PARSER.parse_args()
 
@@ -265,6 +277,6 @@ if __name__ == '__main__':
         ISIN_COMPARE = settings.ISIN_COMPARE
         if ARGS.isin:
             ISIN_COMPARE = [ARGS.isin]
-        diff_report(ARGS.oldest_file, ARGS.newer_file, ISIN_COMPARE)
+        diff_report(ARGS.oldest_file, ARGS.newer_file, ISIN_COMPARE, ARGS.html)
 
     sys.exit(0)
