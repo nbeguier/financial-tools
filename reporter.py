a5010a2768a3b4fcd65983c715791a799d18e66a
Nicolas Béguier <nicolas.beguier@adevinta.com>
2023-07-03 14:09:34 +0200

    Add diff3 for report.py

--- reporter.py
+++ reporter.py
@@ -11,6 +11,8 @@ Written by Nicolas BEGUIER (nicolas_beguier@hotmail.com)
 from argparse import ArgumentParser
 from datetime import datetime
 import json
+import glob
+import os
 import sys
 from pathlib import Path
 
@@ -26,7 +28,7 @@ except ImportError:
 # Debug
 # from pdb import set_trace as st
 
-VERSION = '3.0.0'
+VERSION = '3.1.0'
 
 def get_sign(value):
     """
@@ -191,6 +193,99 @@ def diff_report(oldest_file, newer_file, isin_compare, is_html):
     if is_html:
         print('</body></html>')
 
+
+def get_negative_values(oldest_file, newer_file, isin_compare):
+    """
+    Check for negative valorisation and return corresponding ISINs
+    """
+    old_reports = load_report(oldest_file, display_report=False)
+    new_reports = load_report(newer_file, display_report=False)
+
+    negative_valorisation_isins = []
+    equal_valorisation_isins = []
+
+    for _isin in isin_compare:
+        if _isin not in old_reports or _isin not in new_reports:
+            continue
+        old_report = old_reports[_isin]
+        new_report = new_reports[_isin]
+
+        # Check if 'LVAL_NORM' is in report and if its value is less than 0
+        if 'LVAL_NORM' in new_report and new_report['LVAL_NORM']['v'] is not None:
+            if float(new_report['LVAL_NORM']['v']) - float(old_report['LVAL_NORM']['v']) < 0:
+                negative_valorisation_isins.append(new_report['isin'])
+            if float(new_report['LVAL_NORM']['v']) == float(old_report['LVAL_NORM']['v']):
+                equal_valorisation_isins.append(new_report['isin'])
+
+    return negative_valorisation_isins, equal_valorisation_isins
+
+
+def diff3_report(directory, isin_compare, is_html):
+    """
+    Compare two report
+    """
+    html_tag = {
+        'bold_in': '',
+        'bold_out': '',
+        'h3_in': '',
+        'h3_out': '',
+        'li_in': '',
+        'li_out': '',
+        'red_in': '',
+        'red_out': '',
+        'green_in': '',
+        'green_out': '',
+        'blue_in': '',
+        'blue_out': '',
+    }
+    if is_html:
+        html_tag = {
+            'bold_in': '<b>',
+            'bold_out': '</b>',
+            'h3_in': '<h3>',
+            'h3_out': '</h3>',
+            'li_in': '<li>',
+            'li_out': '</li>',
+            'red_in': '<span style="color: #f00;">',
+            'red_out': '</span>',
+            'green_in': '<span style="color: #18b724;">',
+            'green_out': '</span>',
+            'blue_in': '<span style="color: #007eff;">',
+            'blue_out': '</span>',
+        }
+
+    # Get a list of all report files in the directory, sorted by creation time
+    report_files = sorted(glob.glob(os.path.join(directory, '*.txt')))
+
+    # Take only the last 7 files
+    report_files = report_files[-7:]
+
+    count_negative_isins = {}
+    for i in range(len(report_files) - 1):
+        negative_isins, equal_isins = get_negative_values(report_files[i], report_files[i+1], isin_compare)
+
+        # Initialize isin in negative_isins
+        for _isin in negative_isins:
+            if _isin not in count_negative_isins:
+                count_negative_isins[_isin] = 0
+
+        # Update count
+        for _isin in count_negative_isins:
+            if _isin in negative_isins:
+                count_negative_isins[_isin] += 1
+            elif _isin not in equal_isins:
+                count_negative_isins[_isin] = 0
+
+    if is_html:
+        print('<html><body>')
+
+    for _isin in count_negative_isins:
+        if count_negative_isins[_isin] >= 2:
+            print(f'{html_tag["h3_in"]}{_isin}: {count_negative_isins[_isin]} days in a row !{html_tag["h3_out"]}')
+    if is_html:
+        print('</body></html>')
+
+
 if __name__ == '__main__':
 
     PARSER = ArgumentParser()
@@ -223,6 +318,14 @@ if __name__ == '__main__':
     DIFF_PARSER.add_argument('--html', action='store_true',\
         help='Output in HTML format', default=False)
 
+    # DIFF3 Arguments
+    DIFF3_PARSER = SUBPARSERS.add_parser('diff3',\
+        help='Diff3 command')
+    DIFF3_PARSER.add_argument('directory', action='store',\
+        help='Directory with reports for comparison')
+    DIFF3_PARSER.add_argument('--html', action='store_true',\
+        help='Output in HTML format', default=False)
+
     ARGS = PARSER.parse_args()
 
     if len(sys.argv) == 1:
@@ -238,5 +341,7 @@ if __name__ == '__main__':
         if ARGS.isin:
             ISIN_COMPARE = [ARGS.isin]
         diff_report(ARGS.oldest_file, ARGS.newer_file, ISIN_COMPARE, ARGS.html)
+    elif sys.argv[1] == 'diff3':
+        diff3_report(ARGS.directory, settings.ISIN_COMPARE, ARGS.html)
 
     sys.exit(0)
